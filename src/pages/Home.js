255483c2a156a9ca4c5254157f5379a83b86641e
Julz <jujasen@hotmail.com>
2021-05-25 13:33:28 +0200

    set up

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+const Home = () => {
+
+    return (
+        <div className="page home">
+            <div className="home__landing">
+                <h1>Din inngangsbillett til boligmarkedet</h1>
+                <h2>Leiligheten for deg som drømmer om egen bolig men ikke har nok egenkapital</h2>
+                
+            </div>
+
+        </div>
+    );
+}
+
+export default Home;
