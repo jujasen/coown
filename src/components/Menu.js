8b7221c727ef0da44b15152cd3ac1686f34c00f6
Julz <jujasen@hotmail.com>
2021-05-28 23:31:28 +0200

    added contact modal to menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,25 +2,40 @@ import { Link } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Logo from '../img/logo.svg';
-import { LinkContainer } from 'react-router-bootstrap'
-
+import { LinkContainer } from 'react-router-bootstrap';
+import ContactModal from './ContactModal';
+import { useState } from 'react';
 const Menu = () => {
 
+    const [contact, setContact] = useState(false);
+
+    
+
     return (
-        <Navbar fixed="top" collapseOnSelect className="menu" variant="dark" bg="dark" expand="lg">
-            <Link to="/"><img className="menu__logo" src={Logo} alt="logo"></img></Link>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                    <LinkContainer to="/">
-                        <Nav.Link eventKey="1" className="menu__link">Hjem</Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer to="/results">
-                        <Nav.Link eventkey="2" className="menu__link">Finn Leilighet</Nav.Link>
-                    </LinkContainer>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+        <>
+            {contact ? <ContactModal onChildClick={() => {
+                setContact(false)
+            }} /> : <Navbar fixed="top" collapseOnSelect className="menu" variant="dark" bg="dark" expand="lg">
+                <Link to="/"><img className="menu__logo" src={Logo} alt="logo"></img></Link>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                        <LinkContainer to="/">
+                            <Nav.Link eventKey="1" className="menu__link">Hjem</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/results">
+                            <Nav.Link eventkey="2" className="menu__link">Finn Leilighet</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/" onClick={() => {
+                            setContact(true);
+                        }}>
+                            <Nav.Link eventkey="3" className="menu__link">Kontakt oss</Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            }
+        </>
     );
 }
 
